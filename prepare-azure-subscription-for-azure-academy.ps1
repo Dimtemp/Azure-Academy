195203784656cc936b2832595f572fb4ab512da2
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2019-05-27 23:16:06 +0200

    Update prepare-azure-subscription-for-azure-academy.ps1

--- prepare-azure-subscription-for-azure-academy.ps1
+++ prepare-azure-subscription-for-azure-academy.ps1
@@ -12,5 +12,9 @@ $location = 'westeurope'
 $studentNames = 'name1', 'name2', 'name3'
 
 #resource groups
-$studentNames | New-AzResourceGroup -loc
+$studentNames | foreach { New-AzResourceGroup -Location $location -Name $_ }
 
+#alerts
+#https://docs.microsoft.com/bs-latn-ba/azure/cost-management/tutorial-acm-create-budgets
+#https://docs.microsoft.com/en-us/powershell/module/azurerm.consumption/New-AzureRmConsumptionBudget?view=azurermps-6.13.0
+New-AzureRmConsumptionBudget -Amount 60 -Name PSBudget -Category Cost -StartDate 2019-01-01 -EndDate 2020-01-01 -TimeGrain Monthly
